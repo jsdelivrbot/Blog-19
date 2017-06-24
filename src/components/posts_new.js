9780cf13b82f89eb54dfa4172fd6316046342544
Glenn-Domin <xaduurv@gmail.com>
2017-06-24 11:27:51 +1000

    Validate form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -38,6 +38,27 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  // console.log(values -> { title: 'asdf', categories: 'asdf', content: 'asdf' })
+  const errors = {};
+
+  // Validate the inputs from 'values'
+  if (!values.title || values.title.length < 3) {
+    errors.title = 'Enter a title that is at least 3 characters!';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some categories';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content';
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If errors has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
